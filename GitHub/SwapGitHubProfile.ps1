55980557e574d8d0e5eb8b18905e49f2ad7e4113
SMFX <38084959+smallfoxx@users.noreply.github.com>
2019-12-14 22:06:33 -0600

    Initial commit of SwapGitHubProfile.ps1

---
+++ GitHub/SwapGitHubProfile.ps1
@@ -0,0 +1,160 @@
+<#
+.SYNOPSIS
+Change profiles for GitHub Desktop
+.DESCRIPTION
+Change login profiles for GitHub Desktop to allow for different accounts.
+.NOTES
+The profiles are swapped by utilizing symbolic directory links to different named profile folders.
+.LINK
+mklink.exe
+https://desktop.github.com/
+#>
+[Cmdletbinding(DefaultParameterSetName="Swap")]
+Param(
+    [Parameter(Position=0)]
+    #The name of the profile to switch to
+    [string]$Profile,
+
+    [Parameter(Position=1)]
+    #Name of the configuration file used to store details about the profile
+    [string]$ConfigFile = "AccountDetails.json",
+
+    [Parameter(Position=2)]
+    #Parent directory where profiles are found
+    [String]$ProfilePath = "$env:USERPROFILE\AppData\Roaming",
+
+    [Parameter(Position=3)]
+    #Beginning of the profile direcotry (appended with '-' and the name of the profile)
+    [string]$ProfilePrefix = "GitHub Desktop",
+
+    [parameter(ParameterSetName="Swap")]
+    #Current version of GIT
+    [string]$GitVer = "2.2.4",
+
+    [parameter(ParameterSetName="Swap")]
+    #The path to the executable used for GIT
+    [string]$GitExe = "$env:USERPROFILE\AppData\Local\GitHubDesktop\app-$GitVer\resources\app\git\cmd\git.exe",
+
+    [parameter(ParameterSetName="Swap")]
+    #The path to the executable used for GitHub Desktop
+    [string]$GitHubDesktop = "C:\Users\live\AppData\Local\GitHubDesktop\GitHubDesktop.exe",
+
+    [parameter(ParameterSetName="Swap")]
+    #Copies the current profile to a new profile and then swaps to it
+    [Alias('Create','Copy')]
+    [switch]$New,
+
+    [parameter(ParameterSetName="ListProfiles",Mandatory=$true)]
+    #List all currently available profiles
+    [switch]$ListAvailable)
+
+Begin {
+    Function NewProfileObj{
+        [Cmdletbinding(DefaultParameterSetName="Current")]
+        Param(
+            [Parameter(Position=0)]
+            [string]$ProfileName = $Profile,
+            [Parameter(Position=1)]
+            [string]$Path = "$ProfilePath\$ProfilePrefix-$ProfileName",
+            [Parameter(ParameterSetName="Current")]
+            [switch]$Current,
+            [Parameter(ParameterSetName="New")]
+            [switch]$NewProfile)
+
+        $ThisProfile = Get-Item $Path
+        $ThisProfile | Add-Member -MemberType NoteProperty -Name ConfigFile "$($ThisProfile.FullName)\$ConfigFile"
+        $ThisProfile | Add-Member -MemberType ScriptProperty -Name Config -Value { Get-Content $This.ConfigFile -Raw | ConvertFrom-Json }
+        $ThisProfile | Add-Member -MemberType ScriptProperty -Name ProfileName -Value { $this.Config.Profile }
+        $ThisProfile | Add-Member -MemberType ScriptMethod -Name SaveConfig -Value { $this.Config | ConvertTo-Json | Set-Content $This.ConfigFile }
+        $ThisProfile | Add-Member -MemberType ScriptMethod -Name UpdateConfig -Value {
+            $This.Config.Name = &$GitExe config --global --get user.name
+            $This.Config.PublicEmail = &$GitExe config --global --get user.email
+        }
+        If ($Current) {
+            $ProfileName = $ThisProfile.ProfileName
+        } elseIf ($NewProfile) {
+            $ThisProfile.Config.Profile = $ProfileName
+            $ThisProfile.SaveConfig()
+        }
+        return $ThisProfile | Where-Object { $_.ProfileName -eq $ProfileName }
+    }
+
+    Function NewGitHubProfile {
+        Param([string]$ProfileName = $Profile,
+            [string]$SourceProfile = "$ProfilePath\$ProfilePrefix")
+
+        $NewProfilePath = "$SourceProfile-$ProfileName"
+        Copy-Item $SourceProfile $NewProfilePath
+        $NewProfile = NewProfileObj -ProfileName $ProfileName -Path $NewProfilePath -NewProfile
+
+        Return $NewProfile
+    }
+
+    Function SwapProfile {
+        param($NewProfile)
+
+        Write-Host "Swapping to [$($NewProfile.ProfileName)]..." -ForegroundColor Green
+
+        If ($GHDFile = Get-Item $GitHubDesktop) {
+            $GHDProc = Get-Process -name "GitHubDesktop" -ErrorAction SilentlyContinue
+            If ($GHDProc) {
+                Write-Host "Stopping existing [$($GHDProc.Count)] processes..." -ForegroundColor Red -BackgroundColor Black
+                $GHDProc | Stop-Process -Force
+            }
+        }
+
+        Write-Host "Saving existing config of [$($CurrentProfile.Config.Profile)]..."
+        $ProfilePath = $CurrentProfile.FullName
+        $CurrentProfile.UpdateConfig()
+        $CurrentProfile.SaveConfig()
+        $global:PrevProfile = $CurrentProfile
+
+        Write-Host "Remove existing link at [$ProfilePath]" -ForegroundColor DarkRed -BackgroundColor Black
+        cmd.exe /c rd "$ProfilePath"
+        Write-Host "Make new link from [$($NewProfile.FullName)]" -ForegroundColor DarkGreen -BackgroundColor Black
+        cmd.exe /c mklink /D "$ProfilePath" "$($NewProfile.FullName)"
+        &$GitExe config --global --replace-all user.name $NewProfile.Config.Name
+        &$GitExe config --global --replace-all user.email $NewProfile.Config.PublicEmail
+
+        If ($GHDProc -and $GHDFile) {
+            Write-Host "Restarting GitHub Desktop with [$($NewProfile.ProfileName)] profile" -ForegroundColor Green -BackgroundColor Black
+            &$GHDFile.FullName
+        }
+        $CurrentProfile = $NewProfile
+    }
+
+    $CurrentProfile = NewProfileObj -Path "$ProfilePath\$ProfilePrefix" -Current
+
+    $PossibleProfiles = Get-ChildItem "$ProfilePath\$ProfilePrefix-*" | ForEach-Object {
+        NewProfileObj -ProfileName ($_.BaseName -replace "\A[^-]+-","") -Path $_.FullName
+    }
+}
+
+Process {
+    If ($PSCmdlet.ParameterSetName -eq 'ListProfiles') {
+        $PossibleProfiles.ProfileName | Where-Object{ $_ -match $Profile }
+    } else {
+        If (-not $Profile -and $PossibleProfiles.Count -eq 2) {
+            $Profile = $PossibleProfiles.ProfileName | Where-Object { $_ -ne $CurrentProfile.ProfileName } | Select-Object -First 1
+        } 
+        if ($Profile) {
+            If ($CurrentProfile.ProfileName -eq $Profile) {
+                Write-Host "Current profile is already [$Profile]." -ForegroundColor Yellow
+            } else {
+                $TargetProfile = $PossibleProfiles | Where-Object { $_.ProfileName -eq $Profile }
+                If ($TargetProfile) {
+                    SwapProfile -NewProfile $TargetProfile
+                } elseif ($New) {
+                    $TargetProfile = NewGitHubProfile
+                    SwapProfile -NewProfile $TargetProfile
+                } else {
+                    Write-Error "Could not find a profile [$Profile]"
+                }
+            }
+        }
+    }
+}
+
+End {
+
+}
